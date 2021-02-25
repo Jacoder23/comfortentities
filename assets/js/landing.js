840d5c58ca8dd259124a7169b869c433369adec0
Jacoder23 <Jacoder23@users.noreply.github.com>
2021-02-26 03:49:48 +0800

    Fixed cookies

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,10 +1,6 @@
-(function() {
-	var COOKIE_NAME = 'splash-page-cookie';
-	$go = $.cookie(COOKIE_NAME);
-	if ($go == null) {
-		$.cookie(COOKIE_NAME, 'test', { path: '/', expires: 20*365 });
-		window.location = "/design.html"
-	}
-	else {
-	}
-});
+var firstTime = localStorage.getItem("first_time");
+if(!firstTime) {
+    // first time loaded!
+    localStorage.setItem("first_time","1");
+    window.location.replace("design.html");
+}
